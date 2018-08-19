af3f2cd434123a8f6d2d25d6d0ad3e5d55f72c08
majj <aidear@163.com>
2018-08-20 07:48:35 +0800

    more oop

--- dataQ_sqlite.nim
+++ dataQ_sqlite.nim
@@ -16,23 +16,32 @@ import parsetoml
 
 import lib.utils
 
-type DataQueue = ref object of RootObj
-    config:TomlValueRef
-    db: DbConn
-    status: int
-
-method  init(this:DataQueue):void {.base.} = 
-    # new(ref) or init(val)? 
-    #this.db =  open("data_queue.db", nil, nil, nil)
-    #echo this.config
+type 
     
-    this.db.exec(sql"PRAGMA synchronous = OFF")
+    DataQueue = ref object of RootObj
+        # ref
+        # Signlton ?
+        config: TomlValueRef
+        db: DbConn
+        status: int
 
-    this.db.exec(sql"Drop table if exists data_queue")
+proc  newDataQueue(config: TomlValueRef): DataQueue = 
+    # new(ref) or init(val)? 
+    #self.db =  open("data_queue.db", nil, nil, nil)
+    #echo self.config
+    
+    let db=open("data_queue.db", nil, nil, nil)
+    let status=1
+    
+    let self = DataQueue(db:db, status:status, config:config)
+    
+    self.db.exec(sql"PRAGMA synchronous = OFF")
 
-    this.db.exec(sql"VACUUM")
+    self.db.exec(sql"Drop table if exists data_queue")
 
-    this.db.exec(sql("""create table data_queue (
+    self.db.exec(sql"VACUUM")
+
+    self.db.exec(sql("""create table data_queue (
          id    INTEGER PRIMARY KEY,
          name  VARCHAR(50) NOT NULL,
          active     INT(11),
@@ -40,49 +49,49 @@ method  init(this:DataQueue):void {.base.} =
          data    VARCHAR(1000),
          dt      datetime
          )"""))
-    #return this
+    return self
   
-method enqueue(this:DataQueue):void {.base.} = 
+method enqueue(self: DataQueue): void {.base.} = 
     echo "enqueue"
     # begin transaction
-    this.db.exec(sql"BEGIN")
+    self.db.exec(sql"BEGIN")
     
     for i in 1..10:
       # call prepare_v2 inside
       let t = "n$1".format(i)
       # convert to JsonNode 
       let v = %* {"a":t}
-      this.db.exec(sql"INSERT INTO data_queue (name, active, val, data, dt) VALUES (?,?,?,?,datetime('now'))",
+      self.db.exec(sql"INSERT INTO data_queue (name, active, val, data, dt) VALUES (?,?,?,?,datetime('now'))",
             "Item#" & $i, 1, sqrt(i.float),$v) # $v json to string
     
     #transaction end
-    this.db.exec(sql"COMMIT")
+    self.db.exec(sql"COMMIT")
     
-method dequeue(this:DataQueue, info_s:string):void {.base.} = 
+method dequeue(self: DataQueue, info_s: string): void {.base.} = 
     echo "dequeue:", info_s
     
-    #this.db.exec(sql"Drop table if exists data_queue")
+    #self.db.exec(sql"Drop table if exists data_queue")
 
-    this.db.exec(sql"VACUUM")    
+    self.db.exec(sql"VACUUM")    
 
-method queue_t(this:DataQueue):void {.base.} = 
+method queue_t(self: DataQueue): void {.base.} = 
 
-    for row in this.db.fastRows(sql"select * from data_queue"):
+    for row in self.db.fastRows(sql"select * from data_queue"):
         
         let data_j = parseJson(row[4])
         echo "$1,$2,$3".format(row[0], getStr(data_j["a"]), row[5])
 
-    let id = this.db.tryInsertId(sql"INSERT INTO data_queue (name, active, val) VALUES (?,?,?)",
+    let id = self.db.tryInsertId(sql"INSERT INTO data_queue (name, active, val) VALUES (?,?,?)",
           "Item#1001", 1001, sqrt(1001.0))
           
-    echo "Inserted item: ", this.db.getValue(sql"SELECT name FROM data_queue WHERE id=?", id)
+    echo "Inserted item: ", self.db.getValue(sql"SELECT name FROM data_queue WHERE id=?", id)
 
-proc main(config:TomlValueRef):void = 
+proc main(config: TomlValueRef): void = 
 
     
-    let dataQ = DataQueue(db:open("data_queue.db", nil, nil, nil), status:1, config:config)
+    #let dataQ = DataQueue(db:open("data_queue.db", nil, nil, nil), status:1, config:config)
     
-    dataQ.init()
+    let dataQ: DataQueue = newDataQueue(config)
     
     dataQ.enqueue()
     
