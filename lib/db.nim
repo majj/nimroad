d1f79ccdc7aedc18ddf8d045d34681fc4385c6a6
majj <aidear@163.com>
2018-09-11 18:32:25 +0800

    refactoring

--- lib/db.nim
+++ lib/db.nim
@@ -1,3 +1,6 @@
+# db.nim
+
+# redis
 
 import asyncdispatch
 
@@ -15,8 +18,8 @@ type
 proc newDB*(config: TomlValueRef): DB = 
     
     let redis_conf = config["redis"]
-    let redis_host = parsetoml.getStr(redis_conf["host"],"localhost")
-    let redis_port = parsetoml.getInt(redis_conf["port"],6379)
+    let redis_host = parsetoml.getStr(redis_conf["host"], "localhost")
+    let redis_port = parsetoml.getInt(redis_conf["port"], 6379)
 
     try:
         let redc = waitFor redis.openAsync(redis_host, Port(redis_port))        
@@ -29,3 +32,16 @@ proc set*(self: DB, key:string, data: string):void =
         
 proc push*(self: DB, key:string, data: string):void = 
     discard waitFor self.redc.lPush(key, data)
+    
+proc reconnect(self: DB):bool = 
+
+    let redis_host = parsetoml.getStr(self.config["host"], "localhost")
+    let redis_port = parsetoml.getInt(self.config["port"], 6379)
+    
+    try:
+        self.redc = waitFor redis.openAsync(redis_host, Port(redis_port)) 
+        self.enable = true
+        return true
+    except:
+        self.enable = false
+        return false
